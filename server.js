848a5ac2ad3d7b70db6c6131e02d3773db35b161
vago <vago@vago.vago>
2020-09-01 21:05:42 -0300

    server

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const app = express();
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
 app.use(express.static('./dist/sgm'));
 app.use('*', (req, res) => 
